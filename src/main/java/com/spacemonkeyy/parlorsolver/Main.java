3542ef06d99e3740fa7a169fd50a8934cca38cb3
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2026-03-26 11:58:23 -0400

    Rewrite main function

--- src/main/java/com/spacemonkeyy/parlorsolver/Main.java
+++ src/main/java/com/spacemonkeyy/parlorsolver/Main.java
@@ -1,19 +1,35 @@
 package com.spacemonkeyy.parlorsolver;
 
-import com.spacemonkeyy.parlorsolver.parsing.Parser;
 import com.spacemonkeyy.parlorsolver.puzzle.PuzzleInput;
+import com.spacemonkeyy.parlorsolver.puzzle.PuzzleSolution;
+import com.spacemonkeyy.parlorsolver.solver.Solver;
+import com.spacemonkeyy.parlorsolver.value.BoxColor;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter statement:");
-            String statement = scanner.nextLine();
-            PuzzleInput input = new PuzzleInput(List.of(statement), List.of(), List.of());
-            System.out.println(Parser.parse(input));
+
+        Map<BoxColor, List<String>> map = new HashMap<>();
+
+        for (BoxColor color : BoxColor.values()) {
+            map.put(color, new ArrayList<>());
+            System.out.printf("Enter statements on the %s box:\n", color.name().toLowerCase());
+
+            while (true) {
+                String line = scanner.nextLine();
+                line = line.toUpperCase();
+                if (line.isEmpty()) {
+                    break;
+                }
+                map.get(color).add(line);
+            }
         }
+
+        PuzzleInput input = new PuzzleInput(
+            map.get(BoxColor.BLUE), map.get(BoxColor.WHITE), map.get(BoxColor.BLACK));
+        PuzzleSolution solution = Solver.solve(input);
+        System.out.println(solution);
     }
 }
--- src/test/java/com/spacemonkeyy/parlorsolver/VariationTests.java
+++ src/test/java/com/spacemonkeyy/parlorsolver/VariationTests.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
